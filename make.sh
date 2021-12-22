1886dd7e05b8f1239dcf6a2bd865d20ce7ce0ae4
Artem Dolgih <artemdolgih@hotmail.com>
2021-12-22 09:47:08 +0500

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 module add mpi
-#module add  mpi/mpich-3.2-x86_64
+module add  mpi/mpich-3.2-x86_64
 rm *.out
 mpicc $1 -o filename -lm -O3
 sbatch -n ${2:-4} -t 1 -p debug --wrap "mpiexec ./filename"
